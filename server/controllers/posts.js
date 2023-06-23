abe898b6b16f042443a7d9e78b8cbdd5f705ed30
Keerty Verma <keertyverma1303@gmail.com>
2023-06-23 13:07:56 +0530

    configure basic routes

---
+++ server/controllers/posts.js
@@ -0,0 +1,5 @@
+const getAllPosts = (req, res) => {
+  res.send("all posts...");
+};
+
+export { getAllPosts };
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
 import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
 
 config();
 const PORT = process.env.PORT || 5000;
@@ -11,9 +12,8 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+// configure routes
+app.use("/api/posts", postRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URI)
